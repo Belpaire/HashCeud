97480db2a7084ed45c1445f7cf3d24094ad1c9a6
Tarranoth <belpairebram@hotmail.com>
2020-02-20 18:58:48 +0100

    Classes

---
+++ Library.py
@@ -0,0 +1,6 @@
+class Library :
+    def __init__(self,id,books,sign_time,scan_books):
+        self.id=id
+        self.books=books
+        self.sign_time=sign_time
+        self.scan_books=scan_books

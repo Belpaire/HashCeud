120ee1c8f6df0caf7bfaba61aa6db34128cf7fc1
Tarranoth <belpairebram@hotmail.com>
2020-02-20 20:28:55 +0100

    Updated My methods

--- Library.py
+++ Library.py
@@ -1,12 +1,19 @@
+def mySorter(inputBook,already_read):
+    if inputBook.id in already_read:
+        return 0
+    else:
+        return inputBook.score
+
 class Library :
     def __init__(self,id,books,sign_time,scan_books):
         self.id=int(id)
         self.books=books
         self.sign_time=int(sign_time)
         self.scan_books=int(scan_books)
-    def scoresOfSignTime(self):
-        self.books.sort(key=lambda x: x.score,reverse=True)
+    def scoresOfSignTime(self,already_read):       
+        self.books.sort(key=lambda x: mySorter(x,already_read) ,reverse=True)
         return sum(map(lambda x: x.score,self.books[:self.scan_books]))
-    def get_best_books(self):
-        self.books.sort(key=lambda x: x.score,reverse=True)
-        return self.books
+    def get_best_books(self,already_read):
+        self.books.sort(key=lambda x: mySorter(x,already_read),reverse=True)
+        return self.books[:self.scan_books]
+
